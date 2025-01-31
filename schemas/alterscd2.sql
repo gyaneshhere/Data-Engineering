84bd717790cfbe1b39707b6ec47f6444c7dc5357
Gyanesh <gyaneshhere@users.noreply.github.com>
2025-01-31 10:27:34 -0600

    Create alterscd2.sql
    Alter table statement for adding current flag End data and ChangedField

---
+++ schemas/alterscd2.sql
@@ -0,0 +1,3 @@
+alter table schema.Dim_Product add column Current_Flag varchar(1);
+alter table schema.Dim_Product add column End_date date;
+alter table schema.Dim_Product add column Changed_Field varchar(100);
