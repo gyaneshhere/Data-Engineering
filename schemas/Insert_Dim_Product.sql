1b954aef16571c1a8fe84f61b772ebfee3ca5a2d
Gyanesh <gyaneshhere@users.noreply.github.com>
2025-01-31 10:31:04 -0600

    Create Insert_Dim_Product.sql
    DML Statement for Product Dimension Table

---
+++ schemas/Insert_Dim_Product.sql
@@ -0,0 +1,8 @@
+INSERT INTO product (Product_ID, Category_Name, Sub_Category_Name, Brand, Feature_Desc) VALUES
+  (1001, 'Apparel', 'Shirt', 'Van Heusen', 'Van-M'),
+  (1002, 'Apparel', 'Shirt', 'Van Heusen', 'Van-L'),
+  (1003, 'Apparel', 'Shirt', 'Arrow', 'Arr-XL'),
+  (1004, 'Footwear', 'Shoe', 'Nike', 'Nike-8'),
+  (1005, 'Footwear', 'Shoe', 'Nike', 'Nike-9'),
+  (1006, 'Footwear', 'Shoe', 'Adidas', 'Adidas-10'),
+  (1007, 'Footwear', 'Shoe', 'Adidas', 'Adidas-7');
